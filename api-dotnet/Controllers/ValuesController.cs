a1dd9b4d2fb480592f8a8d82e85da38871bf1722
Dat Le <dat.le@sap.com>
2017-10-05 10:37:33 +0700

    Reserved domain https://regex-tester-api-dotnet.now.sh/
    Change-Id: I143f091b1504b4b8de61353c9dc246543c6f5cf6

---
+++ api-dotnet/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RegExTester.Api.DotNet.Controllers
+{
+    [Route("api")]
+    public class HomeController : Controller
+    {
+        // GET api/
+        [HttpGet]
+        public string Get()
+        {
+            return "RegExTester.Api.DotNet";
+        }
+    }
+}
