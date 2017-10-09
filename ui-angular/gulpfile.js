c750cfd3c9497c489d6f6dd0e19cfb416c956505
Dat Le <dat.le@sap.com>
2017-10-09 17:36:24 +0700

    Fixed error 404 on Github page.
    Added  Gulp commands to build app.
    
    Change-Id: I53c761d84d7211033c009ad524071fd954becfaf

---
+++ ui-angular/gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var exec = require('child_process').exec;
+var rename = require('gulp-rename');
+
+gulp.task('ng-build', function (cb) {
+  exec('ng build', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+})
+
+gulp.task('adapt-github-page', ['ng-build'], function () {
+  return gulp.src('dist/index.html')
+    .pipe(rename('404.html'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', ['adapt-github-page'], function () {
+  return gulp.src('dist/**.*')
+    .pipe(gulp.dest('../../regextester.github.io'));
+});
